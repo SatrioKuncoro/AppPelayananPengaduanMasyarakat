b6513aa75c2ae7f7cc5c275e630c376c7e3b5ee0
JunjungHasudungan <junjunghasudungan43@gmail.com>
2022-10-11 12:12:23 +0700

    penambahan pada bagian migration flight

---
+++ database/seeders/FlightSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Flight;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FlightSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $flights = [ 
+            [
+                // panggil nama protertiesnya dan berikan nilai
+                'id'    => 1,
+                'name'  => 'Pending'
+            ],
+            []
+         ];
+
+        // memanggil nama clas yang akaan diberi benih
+        // Flight
+        
+
+    }
+}
